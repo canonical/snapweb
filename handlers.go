3a268f8e098daea014187f2707317e4e7560447d
Michael Frey <michael.frey@canonical.com>
2014-11-12 13:38:01 -0500

    Initial golang server for Snappy

---
+++ handlers.go
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2014 Canonical Ltd.
+ *
+ * Authors:
+ * Michael Frey: michael.frey@canonical.com
+ *
+ * This file is part of snappy.
+ *
+ * usensord is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * usensord is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package main
+
+import (
+	"net/http"
+	"log"
+)
+
+
+func InitURLHandlers(log *log.Logger) {
+
+	log.Println("Initializing HTTP handlers...")
+
+	http.HandleFunc("/", handleMainPage)
+	http.HandleFunc("/admin", handleAdminPage)
+	http.HandleFunc("/services", handleServicesPage)
+	http.HandleFunc("/store", handleStorePage)
+
+}
---
+++ snappyd.go
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2014 Canonical Ltd.
+ *
+ * Authors:
+ * Michael Frey: michael.frey@canonical.com
+ *
+ * This file is part of snappy.
+ *
+ * usensord is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * usensord is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package main
+
+import (
+	"log"
+	"os"
+	"net/http"
+)
+
+var (
+	logger *log.Logger
+	httpAddr     string 
+)
+
+func init() {
+	logger = log.New(os.Stderr, "Snappy: ", log.Ldate|log.Ltime|log.Lshortfile)
+	httpAddr = "localhost:8080"
+}
+
+func main() {
+
+	InitURLHandlers(logger)
+
+	logger.Println("Snappy starting...")
+
+	if err := http.ListenAndServe(httpAddr, nil); err != nil {
+		logger.Printf("http.ListendAndServer() failed with %s\n", err)
+	}
+
+}
