f6baf3e0eb90b80b8f194e730ab80123bfe36a08
Stephen Stewart <stephen.stewart@canonical.com>
2014-11-28 23:41:15 +0000

    unclosed script tag

--- build.sh
+++ build.sh
@@ -5,7 +5,9 @@ set -e
 builddir=$(mktemp -d)
 trap 'rm -rf "$builddir"' EXIT
 
-cp -r www/public www/templates pkg/. $builddir
+cp -r pkg/. $builddir
+mkdir $builddir/www
+cp -r www/public www/templates $builddir/www
 cd $builddir
 
 go build launchpad.net/clapper/cmd/snappyd
