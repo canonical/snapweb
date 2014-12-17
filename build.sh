cac3ee1acae52c3c7d5fcc7789ffe08f16d40dca
Sergio Schvezov <sergio.schvezov@canonical.com>
2014-12-17 11:35:19 -0300

    Removing cgo code and allowing to build for armhf

--- build.sh
+++ build.sh
@@ -10,6 +10,11 @@ mkdir $builddir/www
 cp -r www/public www/templates www/mock-api $builddir/www
 cd $builddir
 
+if [ "$GOARCH" = "arm" ]; then
+	sed -i 's/architecture: amd64/architecture: armhf/' \
+		$builddir/meta/package.yaml
+fi
+
 go build launchpad.net/clapper/cmd/snappyd
 
 cd -
