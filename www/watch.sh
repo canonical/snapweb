b5dbe39b1f1f9a684e2242cc2c68b838a20f64e4
Robin Winslow <robin.winslow@canonical.com>
2015-01-08 12:12:02 +0000

    Add watcher script to copy files to snappy on the fly

---
+++ www/watch.sh
@@ -0,0 +1,3 @@
+while inotifywait -e close_write templates; do scp -r -P 8022 templates ubuntu@localhost:/apps/snappyd/current/www/.; done &
+while inotifywait -e close_write public; do scp -r -P 8022 public ubuntu@localhost:/apps/snappyd/current/www/.; done &
+while inotifywait -e close_write mock-api; do scp -r -P 8022 mock-api ubuntu@localhost:/apps/snappyd/current/www/.; done
