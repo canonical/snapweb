6499838378038a20f91451b26c8a5ef3e066c6ba
Stephen Stewart <stephen.stewart@canonical.com>
2015-01-13 21:27:35 +0000

    add back search, hook up to api

--- www/src/js/core/search/views/index.js
+++ www/src/js/core/search/views/index.js
@@ -1,40 +1,34 @@
 YUI.add('iot-views-search', function(Y) {
   'use strict';
 
-  var views = Y.namespace('iot.views.search');
+  var tmpls = Y.namespace('iot.tmpls');
+  var mu = new Y.Template();
 
-  views.Index = Y.Base.create('search', Y.View, [], {
+  var SearchView = Y.Base.create('search', Y.View, [], {
 
-    initializer: function() {
-
-      this.listView = new views.List({
-        list: this.get('list')
-      });
-
-      this.listView.addTarget(this);
-    },
-
-    destructor: function() {
-      this.listView.destroy();
-
-      delete this.listView;
-    },
+    template: mu.revive(tmpls.search.list.compiled),
 
     render: function() {
-      document.body.scrollTop = document.documentElement.scrollTop = 0;
-      var content = Y.one(Y.config.doc.createDocumentFragment());
-      content.append(this.listView.render().get('container'));
 
+      document.body.scrollTop = document.documentElement.scrollTop = 0;
+
+      var list = this.get('modelList');
+      var listData = list.map(function(snap) {
+        return snap;
+      });
+      var content = this.template(listData);
       this.get('container').setHTML(content);
 
       return this;
     }
   });
 
+  Y.namespace('iot.views').search = SearchView;
+
 }, '0.0.1', {
   requires: [
     'view',
     'io-base',
-    'iot-views-search-list'
+    't-tmpls-search-list'
   ]
 });
