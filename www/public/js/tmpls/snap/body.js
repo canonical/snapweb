fc1065d27438f6acf931e6c98c38e12a68ba2452
Anthony Dillon <anthony.dillon@canonical.com>
2015-01-14 10:19:30 +0000

    Finished styling of the app details page

--- www/public/js/tmpls/snap/body.js
+++ www/public/js/tmpls/snap/body.js
@@ -1,7 +1,11 @@
 YUI.add("t-tmpls-snap-body", function(Y) { Y.namespace("iot.tmpls.snap.body").compiled = function (Y, $e, data) {
 var $b='', $v=function (v){return v || v === 0 ? v : $b;}, $t='<div class="row-hero app-details">\n  <div class="inner-wrapper">\n    <div class="eight-col">\n      <img src="'+
 $e($v( this.icon_url ))+
-'" width="96" height="96" class="app__icon" />\n      <div class="app__details">\n        <h1 class="app__details-title">'+
+'" width="96" height="96" class="app__icon" />\n      <div class="app__details">\n        <h1 class="app__details-title';
+ if(this.title.length > 25) { 
+$t+=' app__details-title--smaller';
+ } 
+$t+='">'+
 $e($v( this.title ))+
 '</h1>\n        <p>By <a href="'+
 $e($v( this.support_url ))+
@@ -17,13 +21,41 @@ $e($v( value ))+
  }); 
 $t+='\n      </ul>\n    </div>\n    <div class="four-col last-col ">\n      <p>Rating: '+
 $e($v( this.ratings_average ))+
-'/5</p>\n      <a href="" class="link-cta-positive">Install</a>\n      <p class="right">'+
+'/5</p>\n      <p>';
+ if (this.price > 0) { 
+$t+='&dollar;'+
+$e($v( this.price ))+
+'';
+ } else { 
+$t+='Free';
+ } 
+$t+='</p>\n      <a href="" class="link-cta-positive">Install</a>\n      <p class="right">';
+ if (this.binary_filesize > 1000) { 
+$t+=''+
+$e($v( this.binary_filesize / 1000 ))+
+' KB';
+ } else { 
+$t+=''+
 $e($v( this.binary_filesize ))+
-' Bits</p>\n    </div>\n  </div>\n</div>\n\n<div class="row app__details-nav">\n  <div class="inner-wrapper">\n    <nav class="twelve-col ">\n      <ul class="inline-icons">\n        <li><a class="active" href="/apps/'+
+' Bits';
+ } 
+$t+='</p>\n    </div>\n  </div>\n</div>\n\n<div class="row app__details-nav">\n  <div class="inner-wrapper">\n    <nav class="twelve-col no-margin-bottom ">\n      <ul class="inline-icons">\n        <li><a ';
+ if(/[^/]+$/.exec(location.pathname)[0] != 'reviews' && /[^/]+$/.exec(location.pathname)[0] != 'settings') { 
+$t+='class="active" ';
+ } 
+$t+='href="/apps/'+
 $e($v( this.name ))+
-'/details">Details</a></li>\n        <li><a href="/apps/'+
+'/details">Details</a></li>\n        <li><a ';
+ if (/[^/]+$/.exec(location.pathname)[0] == 'reviews') { 
+$t+='class="active" ';
+ } 
+$t+='href="/apps/'+
 $e($v( this.name ))+
-'/reviews">Reviews</a></li>\n        <li><a href="/apps/'+
+'/reviews">Reviews</a></li>\n        <li><a ';
+ if (/[^/]+$/.exec(location.pathname)[0] == 'settings') { 
+$t+='class="active" ';
+ } 
+$t+='href="/apps/'+
 $e($v( this.name ))+
 '/settings">Settings</a></li>\n      </ul>\n    </nav>\n  </div>\n</div>';
 return $t;
--- www/public/js/tmpls/snap/reviews.js
+++ www/public/js/tmpls/snap/reviews.js
@@ -1,4 +1,4 @@
 YUI.add("t-tmpls-snap-reviews", function(Y) { Y.namespace("iot.tmpls.snap.reviews").compiled = function (Y, $e, data) {
-var $b='', $v=function (v){return v || v === 0 ? v : $b;}, $t='<div class="row reviews">\n  \t<div class="inner-wrapper">\n  \t\t<p>Fake app reviews</p>\n\t</div>\n</div>';
+var $b='', $v=function (v){return v || v === 0 ? v : $b;}, $t='\n<div class="row reviews">\n  \t<div class="inner-wrapper">\n  \t\t<h2>Reviews</h2>\n  \t\t<h3>No reviews available at the moment.</h3>\n\t</div>\n</div>';
 return $t;
 }});
