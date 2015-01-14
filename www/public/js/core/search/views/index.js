5fe5426f83b4de92cfc74fac1bcb6f4cea72c37d
Stephen Stewart <stephen.stewart@canonical.com>
2015-01-14 15:33:52 +0000

    search as panel

--- www/public/js/core/search/views/index.js
+++ www/public/js/core/search/views/index.js
@@ -13,9 +13,20 @@ YUI.add('iot-views-search', function(Y) {
       document.body.scrollTop = document.documentElement.scrollTop = 0;
 
       var list = this.get('modelList');
+
+      var queryString = this.get('queryString');
+      console.log('searchString');
+
+      if (list.isEmpty()) {
+        Y.one('.search-results')
+        .setHTML('<p>Sorry, no results found for "' + queryString + '"');
+        return this;
+      }
+
       var listData = list.map(function(snap) {
         return snap;
       });
+
       var content = this.template(listData);
       Y.one('.search-results').setHTML(content);
 
