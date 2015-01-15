1727c0b9539073f916ddc62f25a888fe9a41bd2d
Anthony Dillon <anthony.dillon@canonical.com>
2015-01-15 17:03:59 +0000

    Launch launchable apps from homepage screen

--- www/public/js/tmpls/home.js
+++ www/public/js/tmpls/home.js
@@ -1,7 +1,7 @@
 YUI.add("t-tmpls-home", function(Y) { Y.namespace("iot.tmpls.home").compiled = function (Y, $e, data) {
 var $b='', $v=function (v){return v || v === 0 ? v : $b;}, $t='<div class="row row-hero strip-trans">\n    <div class="inner-wrapper">\n        <section id="apps">\n            ';
  if (this.length === 0) { 
-$t+='\n            <div class="one-col">\n                <img src="/public/images/picto-upload-warmgrey.png" alt="Get some apps here" />\n            </div>\n            <div class="ten-col last-col header--get-apps">\n                <h1>No apps installed</h1>\n                <p><a href="/store">Search the store to get some now &rsaquo;</a></p>\n            </div>\n            ';
+$t+='\n            <div class="one-col">\n                <img src="/public/images/picto-upload-warmgrey.png" alt="Get some apps here" />\n            </div>\n            <div class="ten-col last-col header--get-apps">\n                <h1>No apps installed</h1>\n                <p><a href="/store">Visit the store to get some &rsaquo;</a></p>\n            </div>\n            ';
  } else { 
 $t+='\n            <h1>Installed apps</h1>\n            <ul class="list--apps">\n              ';
  Y.Array.each(this, function(app, index) { 
@@ -11,7 +11,11 @@ $t+='last-col';
  } 
 $t+='">\n                    <a href="'+
 $e($v( app.url ))+
-'" class="link--app">\n                        <img\n                            src="'+
+'" class="link--app"\n                        ';
+ if (app.launchable) { 
+$t+=' target="_blank" ';
+ } 
+$t+='\n                    >\n                        <img\n                            src="'+
 $e($v( app.iconUrl || '/public/images/app-placeholder.svg' ))+
 '"\n                            alt="'+
 $e($v( app.name ))+
