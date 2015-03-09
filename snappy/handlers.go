9909cba13e0ff22463d2efef17913f7185c2560b
Sergio Schvezov <sergio.schvezov@canonical.com>
2015-03-09 16:15:38 -0300

    Minor refactor

--- snappy/handlers.go
+++ snappy/handlers.go
@@ -4,62 +4,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"strconv"
-	"strings"
 
 	"github.com/gorilla/mux"
-
-	"launchpad.net/snappy/snappy"
 )
-
-type snapPkg struct {
-	Name      string          `json:"name"`
-	Version   string          `json:"version"`
-	Installed bool            `json:"installed"`
-	Type      snappy.SnapType `json:"type"`
-	UIPort    uint64          `json:"ui_port,omitempty"`
-	UIUri     string          `json:"ui_uri,omitempty"`
-}
 
 type handler struct{}
 
 func NewHandler() *handler {
 	return &handler{}
-}
-
-func snapQueryToPayload(snapQ snappy.Part) (snap snapPkg) {
-	snap.Name = snapQ.Name()
-	snap.Version = snapQ.Version()
-	snap.Installed = snapQ.IsInstalled()
-	snap.Type = snapQ.Type()
-
-	if snap.Type == snappy.SnapTypeApp || snap.Type == snappy.SnapTypeFramework {
-		if snapInstalled, ok := snapQ.(*snappy.SnapPart); ok {
-			port, uri := uiAccess(snapInstalled.Services())
-			snap.UIPort = port
-			snap.UIUri = uri
-		}
-	}
-
-	return snap
-}
-
-func uiAccess(services []snappy.Service) (port uint64, uri string) {
-	for i := range services {
-		if ui, ok := services[i].Ports.External["ui"]; ok {
-			ui := strings.Split(ui.Port, "/")
-			if len(ui) == 2 {
-				port, err := strconv.ParseUint(ui[0], 0, 64)
-				if err != nil {
-					return 0, ""
-				}
-
-				return port, ui[1]
-			}
-		}
-	}
-
-	return 0, ""
 }
 
 func (h *handler) getAll(w http.ResponseWriter, r *http.Request) {
@@ -70,16 +22,15 @@ func (h *handler) get(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	pkgName := vars["pkg"]
 
-	if snapQ := snappy.ActiveSnapByName(pkgName); snapQ == nil {
+	payload, err := packagePayload(pkgName)
+	if err != nil {
 		http.NotFound(w, r)
 		return
-	} else {
-		snap := snapQueryToPayload(snapQ)
-		enc := json.NewEncoder(w)
-		fmt.Printf("%#v\n", snap)
-		if err := enc.Encode(snap); err != nil {
-			fmt.Fprint(w, "Error")
-		}
+	}
+
+	enc := json.NewEncoder(w)
+	if err := enc.Encode(payload); err != nil {
+		fmt.Fprint(w, "Error")
 	}
 }
 
