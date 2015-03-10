0cdc3f9bde6328b97b687692d40a112d0b6cd7df
Sergio Schvezov <sergio.schvezov@canonical.com>
2015-03-10 11:06:42 -0300

    Remove yet to be used code

--- snappy/handlers.go
+++ snappy/handlers.go
@@ -34,39 +34,6 @@ func (h *handler) get(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (h *handler) add(w http.ResponseWriter, r *http.Request) {
-	decoder := json.NewDecoder(r.Body)
-
-	items := make(map[string]string)
-
-	if err := decoder.Decode(&items); err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, fmt.Sprintf("Error: %s", err))
-		return
-	}
-
-	for k, v := range items {
-		switch k {
-		case "package":
-			fmt.Println(v)
-			http.NotFound(w, r)
-			return
-		default:
-			http.NotFound(w, r)
-			return
-		}
-	}
-
-	w.WriteHeader(http.StatusCreated)
-}
-
-func (h *handler) remove(w http.ResponseWriter, r *http.Request) {
-	// Get the Key.
-	vars := mux.Vars(r)
-	pkgName := vars["pkg"]
-	fmt.Println(pkgName)
-}
-
 func (h *handler) MakeMuxer(prefix string) http.Handler {
 	var m *mux.Router
 
@@ -78,10 +45,6 @@ func (h *handler) MakeMuxer(prefix string) http.Handler {
 
 	// Get all of packages.
 	m.HandleFunc("/", h.getAll).Methods("GET")
-	// Add a package
-	m.HandleFunc("/", h.add).Methods("POST")
-	// Remove a package
-	m.HandleFunc("/{pkg}", h.remove).Methods("DELETE")
 
 	// get specific package
 	m.HandleFunc("/{pkg}", h.get).Methods("GET")
