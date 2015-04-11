17bd62ec4e5cd5e1b8c3da233b82d7a95824ad87
Stephen Stewart <stephen.stewart@canonical.com>
2015-04-11 16:14:07 +0100

    snap sub views working

--- www2/src/js/app.js
+++ www2/src/js/app.js
@@ -9,48 +9,11 @@ if (window.__agent) {
   window.__agent.start(Backbone, Marionette);
 }
 var RootView = require('./views/root.js');
-
-var homeController = require('./controllers/home.js');
-var storeController = require('./controllers/store.js');
-var systemController = require('./controllers/system.js');
-var snapController = require('./controllers/snaps.js');
+var router = require('./routers/router.js');
 
 var webdm = new Marionette.Application();
 var rootView = new RootView();
 rootView.render();
-
-// set up some routes with controllers...
-webdm.routers = {};
-
-// hummm...
-webdm.routers.home = new Marionette.AppRouter({
-  controller: homeController,
-  appRoutes: {
-    '': 'index'
-  }
-});
-
-webdm.routers.store = new Marionette.AppRouter({
-  controller: storeController,
-  appRoutes: {
-    'store': 'index'
-  }
-});
-
-webdm.routers.system = new Marionette.AppRouter({
-  controller: systemController,
-  appRoutes: {
-    'system-settings': 'index'
-  }
-});
-
-webdm.routers.snap = new Marionette.AppRouter({
-  controller: snapController,
-  appRoutes: {
-    'snap/:name/': 'snap',
-    'snap/:name/:section': 'section',
-  }
-});
 
 webdm.on('start', function() {
   Backbone.history.start({pushState: true});
