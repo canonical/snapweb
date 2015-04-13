9112ff7a16a38023747fb067056a6b823bc7dc96
Sergio Schvezov <sergio.schvezov@canonical.com>
2015-04-13 15:01:56 -0500

    wip: progress map

---
+++ progress/meter.go
@@ -0,0 +1,80 @@
+package webprogress
+
+import "launchpad.net/snappy/progress"
+
+// WebProgress show progress on the terminal
+type WebProgress struct {
+	progress.Meter
+	total               float64
+	current             float64
+	statusMessage       string
+	notificationMessage string
+	StartedChan         chan bool
+	FinishedChan        chan bool
+}
+
+// NewWebProgress returns a new WebProgress type
+func NewWebProgress() *WebProgress {
+	return &WebProgress{
+		StartedChan:  make(chan bool),
+		FinishedChan: make(chan bool),
+	}
+}
+
+// Start starts showing progress
+func (t *WebProgress) Start(total float64) {
+	t.total = total
+	t.StartedChan <- true
+}
+
+// Set sets the progress to the current value
+func (t *WebProgress) Set(current float64) {
+	t.current = current
+}
+
+// SetTotal set the total steps needed
+func (t *WebProgress) SetTotal(total float64) {
+	t.total = total
+}
+
+// Finished stops displaying the progress
+func (t *WebProgress) Finished() {
+	t.FinishedChan <- true
+	close(t.StartedChan)
+	close(t.FinishedChan)
+}
+
+// Write is there so that progress can implement a Writer and can be
+// used to display progress of io operations
+//
+// This is not needed for web progress.
+func (t *WebProgress) Write(p []byte) (n int, err error) {
+	return 0, nil
+}
+
+// Spin advances a spinner, i.e. can be used to show progress for operations
+// that have a unknown duration
+func (t *WebProgress) Spin(msg string) {
+	t.statusMessage = msg
+}
+
+// Agreed asks the user whether they agree to the given license text
+func (t *WebProgress) Agreed(intro, license string) bool {
+	// TODO needs implementation
+	return true
+}
+
+// Notify the user of miscelaneous events
+func (t *WebProgress) Notify(msg string) {
+	// TODO needs implementation producer/consumer
+	t.notificationMessage = msg
+}
+
+// Progress returns the current progress
+func (t *WebProgress) Progress() float64 {
+	if t.total == 0 {
+		return 0
+	}
+
+	return t.current / t.total * 100
+}
