65ea34990efe58e90a341a5670c0df85fdb34145
Stephen Stewart <stephen.stewart@canonical.com>
2015-04-25 00:05:53 +0100

    header and footer

---
+++ www/src/js/views/layout-main.js
@@ -0,0 +1,38 @@
+// main layout view
+var _ = require('lodash');
+var Backbone = require('backbone');
+var Marionette = require('backbone.marionette');
+var Radio = require('backbone.radio');
+var BannerView = require('./layout-banner.js');
+var FooterView = require('./layout-footer.js');
+var template = require('../templates/layout-main.hbs');
+var chan = Radio.channel('root');
+
+module.exports = Marionette.LayoutView.extend({
+
+  initialize: function() {
+    chan.comply('set:content', this.setContent, this);
+    console.log('hlo');
+  },
+
+  el: '.b-layout',
+
+  template : function() {
+    return template();
+  },
+
+  onRender: function() {
+    this.showChildView('bannerRegion', new BannerView());
+    this.showChildView('footerRegion', new FooterView());
+  },
+
+  setContent: function(content) {
+    this.mainRegion.show(content);
+  },
+
+  regions: {
+    bannerRegion: '.b-layout__banner',
+    mainRegion: '.b-layout__main',
+    footerRegion: '.b-layout__footer'
+  }
+});
