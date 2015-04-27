792837c7d4ad6115ef6f4a304339f12bccb2fbd9
Stephen Stewart <stephen.stewart@canonical.com>
2015-04-27 19:17:46 +0100

    banner, search, fix flexbox bug in ff

--- www/src/js/views/layout-banner.js
+++ www/src/js/views/layout-banner.js
@@ -7,8 +7,35 @@ module.exports = Marionette.ItemView.extend({
 
   className: 'b-banner',
 
-  template : function() {
-    return template();
+  ui: {
+    input: '.b-banner__input',
+    submit: '.b-banner__submit'
+  },
+
+  events: {
+    'click @ui.submit': 'submit'
+  },
+
+  serializeData: function(){
+    var webdm = window.WEBDM;
+    return {
+      'query': window.decodeURI(window.location.search.slice(3)) || '',
+      'name': webdm.NAME,
+      'subname': webdm.SUBNAME,
+      'active': window.location.pathname.split('/')[0]
+    };
+  },
+
+  template : function(data) {
+    return template(data);
+  },
+
+  submit: function(e) {
+    var val = this.ui.input.val();
+
+    if (!val) {
+      e.preventDefault();
+    }
   }
 
 });
