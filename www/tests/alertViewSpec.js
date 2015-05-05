553115a7e87f44064260b5589343cb046bd88934
Stephen Stewart <stephen.stewart@canonical.com>
2015-05-05 13:48:15 +0100

    wip, add tests

---
+++ www/tests/alertViewSpec.js
@@ -0,0 +1,15 @@
+var SnapItemView = require('../src/js/views/alerts.js');
+var Backbone = require('backbone');
+
+describe('AlertView', function() {
+
+  beforeEach(function() {
+    this.view = new AlertView();
+  });
+
+  afterEach(function() {
+    this.view.destroy();
+  });
+
+
+});
