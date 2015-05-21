6c7b7036eb9e1b6cdc26171170f3603efccf4932
Stephen Stewart <stephen.stewart@canonical.com>
2015-05-21 17:28:43 +0100

    fix icon in snap page

--- www/src/js/views/snap-settings.js
+++ www/src/js/views/snap-settings.js
@@ -4,6 +4,7 @@ var Marionette = require('backbone.marionette');
 var template = require('../templates/snap-settings.hbs');
 
 module.exports = Backbone.Marionette.ItemView.extend({
+  className: 'b-snap__tab-content',
   template: function() {
     return template();
   },
