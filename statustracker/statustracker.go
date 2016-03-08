13f38f14b1d96fc82c441f72208f3f3339c9fc10
Steven Wilkin <stevenwilkin@gmail.com>
2016-03-08 18:30:48 +0000

    Introduce a simple installation status tracker

---
+++ statustracker/statustracker.go
@@ -0,0 +1,123 @@
+/*
+ * Copyright (C) 2016 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+// Package statustracker enables the tracking of snap installation and removal.
+//
+// Once a snap has been marked as "installing" it will remain in that
+// state until it's status as provided by snapd indicates that it is installed
+// on the system. Similarly for removing snaps. Status lifecycle is thus:
+//
+// "uninstalled" -> "installing" -> "installed" -> "uninstalling" and repeat
+package statustracker
+
+import (
+	"sync"
+
+	"github.com/ubuntu-core/snappy/client"
+)
+
+const (
+	// StatusInstalled indicates the package is in an installed state.
+	StatusInstalled = "installed"
+	// StatusUninstalled indicates the package is in an uninstalled state.
+	StatusUninstalled = "uninstalled"
+	// StatusInstalling indicates the package is in an installing state.
+	StatusInstalling = "installing"
+	// StatusUninstalling indicates the package is in an uninstalling state.
+	StatusUninstalling = "uninstalling"
+)
+
+// StatusTracker tracks statuses
+type StatusTracker struct {
+	sync.Mutex
+	statuses map[string]string
+}
+
+// New returns a new status tracker
+func New() *StatusTracker {
+	return &StatusTracker{
+		statuses: make(map[string]string),
+	}
+}
+
+// Status returns the status of the given snap
+func (s *StatusTracker) Status(snap *client.Snap) string {
+	s.Lock()
+	defer s.Unlock()
+
+	status, ok := s.statuses[snapID(snap)]
+	if !ok {
+		return translateStatus(snap)
+	}
+
+	if hasCompleted(status, snap) {
+		delete(s.statuses, snapID(snap))
+		return translateStatus(snap)
+	}
+
+	return status
+}
+
+// TrackInstall tracks the installation of the given snap
+func (s *StatusTracker) TrackInstall(snap *client.Snap) {
+	if isInstalled(snap) {
+		return
+	}
+
+	s.Lock()
+	defer s.Unlock()
+
+	s.statuses[snapID(snap)] = StatusInstalling
+}
+
+// TrackUninstall tracks the removal of the given snap
+func (s *StatusTracker) TrackUninstall(snap *client.Snap) {
+	if !isInstalled(snap) {
+		return
+	}
+
+	s.Lock()
+	defer s.Unlock()
+
+	s.statuses[snapID(snap)] = StatusUninstalling
+}
+
+func snapID(s *client.Snap) string {
+	return s.Name + "." + s.Origin
+}
+
+func isInstalled(s *client.Snap) bool {
+	return s.Status == client.StatusInstalled || s.Status == client.StatusActive
+}
+
+// translate a status from the snappy world into one webdm understands
+func translateStatus(s *client.Snap) string {
+	if isInstalled(s) {
+		return StatusInstalled
+	}
+
+	return StatusUninstalled
+}
+
+// has the tracked process denoted by status completed?
+func hasCompleted(status string, snap *client.Snap) bool {
+	if status == StatusInstalling {
+		return isInstalled(snap)
+	}
+
+	return !isInstalled(snap)
+}
---
+++ statustracker/statustracker_test.go
@@ -0,0 +1,108 @@
+/*
+ * Copyright (C) 2016 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package statustracker
+
+import (
+	"testing"
+
+	"github.com/ubuntu-core/snappy/client"
+
+	. "gopkg.in/check.v1"
+)
+
+func Test(t *testing.T) { TestingT(t) }
+
+type StatusTrackerSuite struct {
+	t *StatusTracker
+}
+
+var _ = Suite(&StatusTrackerSuite{})
+
+func (s *StatusTrackerSuite) SetUpTest(c *C) {
+	s.t = New()
+}
+
+func (s *StatusTrackerSuite) TestTranslateStatus(c *C) {
+	tests := []struct {
+		snapStatus string
+		status     string
+	}{
+		{client.StatusInstalled, StatusInstalled},
+		{client.StatusActive, StatusInstalled},
+		{client.StatusNotInstalled, StatusUninstalled},
+		{client.StatusRemoved, StatusUninstalled},
+	}
+
+	for _, tt := range tests {
+		snap := &client.Snap{Status: tt.snapStatus}
+		c.Assert(translateStatus(snap), Equals, tt.status)
+	}
+}
+
+func (s *StatusTrackerSuite) TestHasCompleted(c *C) {
+	tests := []struct {
+		status     string
+		snapStatus string
+		completed  bool
+	}{
+		{StatusInstalling, client.StatusInstalled, true},
+		{StatusInstalling, client.StatusRemoved, false},
+		{StatusUninstalling, client.StatusRemoved, true},
+		{StatusUninstalling, client.StatusActive, false},
+	}
+
+	for _, tt := range tests {
+		snap := &client.Snap{Status: tt.snapStatus}
+		c.Assert(hasCompleted(tt.status, snap), Equals, tt.completed)
+	}
+}
+
+func (s *StatusTrackerSuite) TestUntrackedSnap(c *C) {
+	snap := &client.Snap{Status: client.StatusInstalled}
+	c.Assert(s.t.Status(snap), Equals, StatusInstalled)
+}
+
+func (s *StatusTrackerSuite) TestTrackInstallAlreadyInstalled(c *C) {
+	snap := &client.Snap{Status: client.StatusInstalled}
+	s.t.TrackInstall(snap)
+	c.Assert(s.t.Status(snap), Equals, StatusInstalled)
+}
+
+func (s *StatusTrackerSuite) TestTrackInstall(c *C) {
+	snap := &client.Snap{Status: client.StatusNotInstalled}
+	s.t.TrackInstall(snap)
+	c.Assert(s.t.Status(snap), Equals, StatusInstalling)
+	// installation completes
+	snap.Status = client.StatusActive
+	c.Assert(s.t.Status(snap), Equals, StatusInstalled)
+}
+
+func (s *StatusTrackerSuite) TestTrackUninstallNotInstalled(c *C) {
+	snap := &client.Snap{Status: client.StatusNotInstalled}
+	s.t.TrackUninstall(snap)
+	c.Assert(s.t.Status(snap), Equals, StatusUninstalled)
+}
+
+func (s *StatusTrackerSuite) TestTrackUninstall(c *C) {
+	snap := &client.Snap{Status: client.StatusInstalled}
+	s.t.TrackUninstall(snap)
+	c.Assert(s.t.Status(snap), Equals, StatusUninstalling)
+	// uninstallation completes
+	snap.Status = client.StatusRemoved
+	c.Assert(s.t.Status(snap), Equals, StatusUninstalled)
+}
