520ec1557762b7ba7562355c297e4ad15ed949c6
Steven Wilkin <stevenwilkin@gmail.com>
2016-07-29 17:29:46 +0100

    Basic tabbed content for settings page

---
+++ www/src/js/views/settings-device.js
@@ -0,0 +1,11 @@
+var Backbone = require('backbone');
+var Marionette = require('backbone.marionette');
+var template = require('../templates/settings-device.hbs');
+
+module.exports = Backbone.Marionette.ItemView.extend({
+  className: 'b-settings__device',
+
+  template: function() {
+    return template();
+  }
+});
--- www/src/js/views/settings.js
+++ www/src/js/views/settings.js
@@ -1,10 +1,63 @@
 // settings view
+var $ = require('jquery');
 var Backbone = require('backbone');
+Backbone.$ = $;
 var Marionette = require('backbone.marionette');
 var template = require('../templates/settings.hbs');
+var SettingsDeviceView = require('./settings-device.js');
+var SettingsProfileView = require('./settings-profile.js');
+var SettingsUsersView = require('./settings-users.js');
+var SettingsUpdatesView = require('./settings-updates.js');
+var SettingsTimeView = require('./settings-time.js');
 
-module.exports = Backbone.Marionette.ItemView.extend({
+module.exports = Backbone.Marionette.LayoutView.extend({
   template: function(model) {
     return template(model);
   },
+
+  ui: {
+    'row': '.b-settings__row'
+  },
+
+  events: {
+    'click @ui.row': 'setActive'
+  },
+
+  regions: {
+    contentRegion: '.b-settings__content'
+  },
+
+  showContent: function(id) {
+    var view;
+    switch (id) {
+      case 'profile':
+        view = new SettingsProfileView();
+        break;
+      case 'users':
+        view = new SettingsUsersView();
+        break;
+      case 'updates':
+        view = new SettingsUpdatesView();
+        break;
+      case 'time':
+        view = new SettingsTimeView();
+        break;
+      case 'device':
+      default:
+        view = new SettingsDeviceView()
+    }
+
+    this.showChildView('contentRegion', view);
+  },
+
+  setActive: function(e) {
+    var id = e.target.getAttribute('id');
+    this.showContent(id);
+    this.$('.b-settings__row').removeClass('b-settings__row_active');
+    this.$('#' + id).addClass('b-settings__row_active');
+  },
+
+  onBeforeShow: function() {
+    this.showContent('device');
+  }
 });
