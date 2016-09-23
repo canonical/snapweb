9f419d83a1330fb9ae969be4d0f1e01219d4c5be
David Barth <david.barth@canonical.com>
2016-09-23 17:27:56 +0200

    button click event support

--- www/src/js/views/init.js
+++ www/src/js/views/init.js
@@ -8,6 +8,19 @@ module.exports = Backbone.Marionette.LayoutView.extend({
 
   className: 'b-layout__container',
 
+  events: {
+    'click @ui.create': 'handleCreate',
+  },
+
+  ui: {
+    create: '.btn-create',
+  },
+
+  handleCreate: function() {
+    console.log('create button pressed');
+    event.preventDefault();
+  },
+
   template : function() {
     console.log('in init.js/template');
     return template();
