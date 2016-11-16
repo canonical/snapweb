5d205894db23abac37e28bba7c5cb5ce1bce05bd
Iftikhar Ahmad <iftikhar.ahmad@canonical.com>
2016-11-16 08:20:48 +0500

    remove duplicate file

--- tests/remote-install-snap.sh
+++
@@ -1,6 +0,0 @@
-set +x
-
-ssh snapweb-ci@162.213.35.217 "if [ -d tmpsnaps ]; then rm -rf tmpsnaps; fi; mkdir tmpsnaps;"
-scp snapweb_0.21.2_amd64.snap snapweb-ci@162.213.35.217:/home/snapweb-ci/tmpsnaps/
-ssh snapweb-ci@162.213.35.217 "sudo snap remove snapweb >/dev/null"
-ssh snapweb-ci@162.213.35.217 "sudo snap install /home/snapweb-ci/tmpsnaps/snapweb_0.21.2_amd64.snap --devmode"
