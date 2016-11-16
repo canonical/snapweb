f97254bf0012d4b16a9090be8a2fa170410bffe5
Justin McPherson <justin.mcpherson@canonical.com>
2016-11-16 11:48:54 +1000

    Adjust style of js

--- www/src/js/views/settings-device.js
+++ www/src/js/views/settings-device.js
@@ -5,7 +5,6 @@ var template = require('../templates/settings-device.hbs');
 var Config = require('../config.js');
 var chan = Radio.channel('root');
 
-
 module.exports = Backbone.Marionette.ItemView.extend({
   className: 'b-settings__device',
 
@@ -27,21 +26,23 @@ module.exports = Backbone.Marionette.ItemView.extend({
                 processData: false,
                 async: true,
                 error: function(jqXHR, textStatus, errorThrown) {
-                  chan.command('alert:error', new Backbone.Model({message: errorThrown}));
+                  chan.command('alert:error',
+                               new Backbone.Model({message: errorThrown}));
                 }
               });
   },
 
-  powerOff: function () {
+  powerOff: function() {
     Backbone.ajax({
                 url: Config.DEVICE_ACTION,
                 contentType: 'application/json',
-                type: "POST",
+                type: 'POST',
                 data: '{ "actionType": "power-off" }',
                 processData: false,
                 async: true,
                 error: function(jqXHR, textStatus, errorThrown) {
-                  chan.command('alert:error', new Backbone.Model({message: errorThrown}));
+                  chan.command('alert:error',
+                               new Backbone.Model({message: errorThrown}));
                 }
               });
   }
