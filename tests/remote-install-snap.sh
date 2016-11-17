341be99e9b77d795d694bafb60c97f0c2d79a247
Iftikhar Ahmad <iahmad9@users.noreply.github.com>
2016-11-17 15:56:08 +0500

    Snapweb selenium tests (#94)

---
+++ tests/remote-install-snap.sh
@@ -0,0 +1,17 @@
+set +x
+
+user=$1
+host=$2
+port=$3
+snap=$4
+if [ -z "$4" ]; then
+	echo "No snap file specified to install, exiting"
+	exit 1
+fi
+
+snap_name="${snap##*/}"
+
+ssh -p $port $user@$host "if [ -d tmpsnaps ]; then rm -rf tmpsnaps; fi; mkdir tmpsnaps;"
+scp -P $port $snap  $user@$host:/home/$user/tmpsnaps/
+ssh -p $port $user@$host "sudo snap remove snapweb >/dev/null"
+ssh -p $port $user@$host "sudo snap install /home/$user/tmpsnaps/$snap_name --devmode"
---
+++ tests/run-tests.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+exitcode=0
+
+if [ $# -lt 3 ]
+  then
+    filename=$(basename -- "$0")
+    echo "Not enough arguments supplied, please provide arguments in following order for DUT"
+    echo "$ ./$filename <user> <IP> <ssh port> <sudo>"
+    echo "If sudo is required, then specify 4th arg as sudo"
+    echo "If want to skip dowloading and installing part, provide 5th arg non empty"
+    exit 1
+fi
+
+user=$1
+host=$2
+port=$3
+sudo=false
+
+if [ $4 = "sudo" ]; then
+	sudo=true
+fi
+
+#skip the downloading if an extra arg is provided, for quick subsequent run locally.
+if [ -z "$5" ]
+  then
+    echo 'Setup the environment before running snapweb selenium test'
+    echo '----------------------------------------------------------'
+    wget -nc http://chromedriver.storage.googleapis.com/2.25/chromedriver_linux64.zip
+    unzip -o chromedriver_linux64.zip
+
+    wget -nc https://github.com/mozilla/geckodriver/releases/download/v0.11.1/geckodriver-v0.11.1-linux64.tar.gz
+    tar -xvzf geckodriver-v0.11.1-linux64.tar.gz
+
+    export PATH=$PATH:$PWD
+
+    echo 'Downloading Standalone Selenium Server...'
+    wget -nc https://selenium-release.storage.googleapis.com/3.0/selenium-server-standalone-3.0.1.jar
+
+#install npm deps
+    npm install webdriverio --save-dev
+    npm install chai --save-dev
+    npm install wdio-mocha-framework --save-dev
+    npm install wdio-junit-reporter --save-dev
+    npm install ssh2 --save-dev
+    npm install node-ssh --save-dev
+fi
+
+echo 'Launching Selenium Server ...'
+java -jar ./selenium-server-standalone-3.0.1.jar > /dev/null 2>&1 &
+pid=$!
+exitcode=$?
+
+sleep 3 # wait for selenium server to up and running.
+
+modules_dir="./node_modules"
+if [ ! -d "$modules_dir" ]; then
+  modules_dir="../node_modules"
+fi
+
+echo 'Run selenium tests for snapweb service on given IP'
+USER=$user HOST=$host PORT=$port SUDO=$sudo $modules_dir/.bin/wdio -b https://$2:4201
+exitcode=$?
+
+trap "kill -9  $pid" $exitcode
+
+exit $exitcode
