7b37d81fb132127cffebcb6cd55226826ff52918
Alexandre Abreu <alexandre.abreu@canonical.com>
2016-11-24 14:45:09 -0500

    Fix selenium tests

--- tests/specs/test-installed-snaps.js
+++ tests/specs/test-installed-snaps.js
@@ -52,18 +52,18 @@ describe('Installed Snaps Page - Verify that', function() {
     it('clicking store link takes the user to store', function() {
 
         snapsPage.store.click();
-        storepage = browser.element('h2=Available snaps')
+        storepage = browser.element('h2=Featured snaps')
         storepage.waitForVisible();
-        expect(storepage.getText(), "Failed to load store page").to.contain('Available snaps');
+        expect(storepage.getText(), "Failed to load store page").to.contain('Featured snaps');
 
     });
 
     it('Browse store button takes the user to store', function() {
 
         snapsPage.browsestore.click();
-        storepage = browser.element('h2=Available snaps')
+        storepage = browser.element('h2=Featured snaps')
         storepage.waitForVisible();
-        expect(storepage.getText(), "Failed to load store page").to.contain('Available snaps');
+        expect(storepage.getText(), "Failed to load store page").to.contain('Featured snaps');
 
     });
 
