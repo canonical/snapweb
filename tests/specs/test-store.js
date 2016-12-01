c15bd6359226b3fb81213c49d811ea10d64eed15
David Barth <david.barth@canonical.com>
2016-12-01 18:18:13 +0100

    store page and private section test

---
+++ tests/specs/test-store.js
@@ -0,0 +1,28 @@
+var assert = require('chai').assert;
+var expect = require('chai').expect;
+
+storePage = require("../pageobjects/store-page.js");
+
+describe('Store Page - Verify that', function() {
+
+    beforeEach(function() {
+        storePage.open();
+    });
+
+    afterEach(function() {});
+
+    it('front page loads correctly', function() {
+        // just opening the storePage in "beforeEach" above does prove it
+        assert(true);
+    });
+
+    it('a private section exists', function() {
+
+        storePage.private.click();
+        privatepage = browser.element('h2=Featured snaps')
+        privatepage.waitForVisible();
+        expect(privatepage.getText(), "Failed to load private section page").to.contain('Private snaps');
+
+    });
+
+});
