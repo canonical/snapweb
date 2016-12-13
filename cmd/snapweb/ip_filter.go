4beee9f62d4ed4b6b8ff04776e2d09636c7f3597
David Barth <david.barth@canonical.com>
2016-12-13 15:03:24 +0100

    wip

---
+++ cmd/snapweb/ip_filter.go
@@ -0,0 +1,49 @@
+type IpFilter struct {
+	BlockByDefault bool
+	AllowedIPs []*net.IPNet
+}
+
+
+
+
+//ipRange - a structure that holds the start and end of a range of ip addresses
+type ipRange struct {
+    start net.IP
+    end net.IP
+}
+
+// inRange - check to see if a given ip address is within a range given
+func inRange(r ipRange, ipAddress net.IP) bool {
+    // strcmp type byte comparison
+    if bytes.Compare(ipAddress, r.start) >= 0 && bytes.Compare(ipAddress, r.end) < 0 {
+        return true
+    }
+    return false
+}
+
+var privateRanges = []ipRange{
+    ipRange{
+        start: net.ParseIP("10.0.0.0"),
+        end:   net.ParseIP("10.255.255.255"),
+    },
+    ipRange{
+        start: net.ParseIP("100.64.0.0"),
+        end:   net.ParseIP("100.127.255.255"),
+    },
+    ipRange{
+        start: net.ParseIP("172.16.0.0"),
+        end:   net.ParseIP("172.31.255.255"),
+    },
+    ipRange{
+        start: net.ParseIP("192.0.0.0"),
+        end:   net.ParseIP("192.0.0.255"),
+    },
+    ipRange{
+        start: net.ParseIP("192.168.0.0"),
+        end:   net.ParseIP("192.168.255.255"),
+    },
+    ipRange{
+        start: net.ParseIP("198.18.0.0"),
+        end:   net.ParseIP("198.19.255.255"),
+    },
+}
