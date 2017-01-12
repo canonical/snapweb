8eb562d703c45a4226bb26590d12a8bfb81c11cc
Alexandre Abreu <alexandre.abreu@canonical.com>
2017-01-12 16:58:54 -0500

    remove leftover test tweak

--- tests/wdio.conf.js
+++ tests/wdio.conf.js
@@ -10,7 +10,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './specs/**/test-store*.js'
+        './specs/**/*.js'
     ],
     // Patterns to exclude.
     exclude: [
