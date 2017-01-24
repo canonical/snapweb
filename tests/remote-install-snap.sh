b4cdd5d457a2026770ff21e31a30de8b70debb72
David Barth <david.barth@canonical.com>
2017-01-24 18:47:15 +0100

    Manually connect interfaces on the test system (#137)

--- tests/remote-install-snap.sh
+++ tests/remote-install-snap.sh
@@ -15,3 +15,6 @@ ssh -p $port $user@$host "if [ -d tmpsnaps ]; then rm -rf tmpsnaps; fi; mkdir tm
 scp -P $port $snap  $user@$host:/home/$user/tmpsnaps/
 ssh -p $port $user@$host "sudo snap remove snapweb >/dev/null"
 ssh -p $port $user@$host "sudo snap install /home/$user/tmpsnaps/$snap_name --devmode"
+# need to manually connect interfaces as snapd won't do it anymore in devmode
+ssh -p $port $user@$host "sudo snap connect snapweb:snapd-control"
+ssh -p $port $user@$host "sudo snap connect snapweb:timeserver-control"
