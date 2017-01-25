6fe75a1e84417e891f2027a75a7228028957edc9
David Barth <david.barth@canonical.com>
2017-01-25 14:53:50 +0100

    initial view

---
+++ www/src/js/routers/webconf-router.js
@@ -0,0 +1,59 @@
+// webconf-router.js
+
+var Backbone = require('backbone');
+var Marionette = require('backbone.marionette');
+
+var homeController = require('../controllers/home.js');
+// var initController = require('../controllers/init.js');
+var searchController = require('../controllers/search.js');
+var storeController = require('../controllers/store.js');
+var settingsController = require('../controllers/settings.js');
+var snapController = require('../controllers/snaps.js');
+var tokenController = require('../controllers/token.js');
+
+module.exports = {
+
+  home: new Marionette.AppRouter({
+    controller: homeController,
+    appRoutes: {
+      '': 'index'
+    }
+  }),
+
+  token: new Marionette.AppRouter({
+    controller: tokenController,
+    appRoutes: {
+      'access-control': 'index'
+    }
+  }),
+
+  store: new Marionette.AppRouter({
+    controller: storeController,
+    appRoutes: {
+      'store': 'index',
+      'store/section/:section': 'section',
+      'search?q=': 'index'
+    }
+  }),
+
+  settings: new Marionette.AppRouter({
+    controller: settingsController,
+    appRoutes: {
+      'settings': 'index'
+    }
+  }),
+
+  snap: new Marionette.AppRouter({
+    controller: snapController,
+    appRoutes: {
+      'snap/:id': 'snap',
+    }
+  }),
+
+  search: new Marionette.AppRouter({
+    controller: searchController,
+    appRoutes: {
+      'search?q=:query': 'query',
+    }
+  })
+};
