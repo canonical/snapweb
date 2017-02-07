542c6f52f4df54b7e539e57996920d6a244bb74f
David Barth <david.barth@canonical.com>
2017-02-07 19:43:14 +0100

    install yarn properly

--- scripts/snap.sh
+++ scripts/snap.sh
@@ -5,9 +5,11 @@
 set -ev
 
 dpkg --add-architecture i386
+apt update
+apt install -y bzr gcc-5-multilib gcc-5-aarch64-linux-gnu gcc-5-arm-linux-gnueabihf gcc-aarch64-linux-gnu gcc-arm-linux-gnueabihf git golang-go libc6-dev:i386 nodejs-legacy wget apt-transport-https
 wget -O- https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 apt update
-apt install -y bzr gcc-5-multilib gcc-5-aarch64-linux-gnu gcc-5-arm-linux-gnueabihf gcc-aarch64-linux-gnu gcc-arm-linux-gnueabihf git golang-go libc6-dev:i386 nodejs-legacy yarn
+apt install yarn
 yarn install
 ./build.sh
