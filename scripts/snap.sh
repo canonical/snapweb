42da96b179db96088ebf50156340b824ce231c7a
David Barth <david.barth@canonical.com>
2017-02-07 21:43:39 +0100

    yarn install twice to work around phantomjs issues

--- scripts/snap.sh
+++ scripts/snap.sh
@@ -11,5 +11,13 @@ wget -O- https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 apt update
 apt install yarn
+# NPM hammers the registry so badly it triggers ECONNERR issues
+# Moving to yarn to resolve that creates another problem with phantomjs
+# To solve the phantomjs problem, we resort to it's -prebuilt variant
+# Here, yarn will initially fail because of phantomjs, then with the help of the phantomjs-prebuilt, it will work
+set +e
+yarn install
+set -e
+# do it again to complete the installation of all modules referenced in package.json <sigh...>
 yarn install
 ./build.sh
