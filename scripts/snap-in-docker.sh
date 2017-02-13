634763233e7eda3fffa80c56291ace5beafc4cd8
David Barth <david.barth@canonical.com>
2017-02-13 16:07:30 +0100

    guard travis-specific chowns, to allow running snap-in-docker on a developer system

--- scripts/snap-in-docker.sh
+++ scripts/snap-in-docker.sh
@@ -7,6 +7,7 @@ set -ev
 docker run -v $GOPATH:/go dbarth/snapweb-builder sh -c 'cd /go/src/github.com/snapcore/snapweb && export GOPATH=/go PATH=/go/bin:$PATH && ./scripts/snap.sh'
 
 #change the permissions to user travis
-sudo chown travis:travis *.snap
-sudo chown -R travis:travis node_modules
-
+if [ ! -z $TRAVIS ]; then
+  sudo chown travis:travis *.snap
+  sudo chown -R travis:travis node_modules
+fi
