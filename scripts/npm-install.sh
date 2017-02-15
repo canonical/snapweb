b01736c6c8d7a72849740876c2ce644ede3464a6
David Barth <david.barth@canonical.com>
2017-02-15 19:42:44 +0100

    simplify run-checks by offloading all the npm setup to a travis-specifc script

---
+++ scripts/npm-install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+
+set -eu
+
+# common npm/yarn install process, to avoid repeating the same workarounds
+npm_install()
+{
+    echo Obtaining npm dependencies
+    set +e
+    yarn install
+    set -e
+    # twice, to cope with phantomjs postinstall issue
+    npm install phantomjs-prebuilt
+    export PHANTOMJS_BIN=`pwd`/node_modules/.bin/phantomjs
+    # third time to fix other postinstall woes
+    npm rebuild node-sass
+}
+
+npm_install
