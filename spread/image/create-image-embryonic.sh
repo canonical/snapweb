a6204f64a79fa272a574e110bd42a1c4e352a8ae
David Barth <david.barth@canonical.com>
2017-02-16 18:36:37 +0100

    fix copyright date

--- spread/image/create-image-embryonic.sh
+++ spread/image/create-image-embryonic.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 Canonical Ltd
+# Copyright (C) 2017 Canonical Ltd
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 3 as
