4897d72cad6c00b763350ac08d8962ccc565f1a3
David Barth <david.barth@canonical.com>
2017-02-16 18:32:52 +0100

    update js unit test suite

--- www/src/js/models/create-user.js
+++ www/src/js/models/create-user.js
@@ -2,7 +2,6 @@
 
 var Backbone = require('backbone');
 var Marionette = require('backbone.marionette');
-var CONFIG = require('../config.js');
 
 module.exports = Backbone.Model.extend({
   url: '/api/v2/create-user',
