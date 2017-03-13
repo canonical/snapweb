9567fcc1092b57754bccd9e0b6c1c9ee67ef3496
David Barth <david.barth@canonical.com>
2017-03-13 18:52:01 +0100

    fix footer; fix selenium tests

--- tests/remote-install-snap.sh
+++ tests/remote-install-snap.sh
@@ -18,6 +18,9 @@ ssh ${SSH_OPTS} "if [ -d tmpsnaps ]; then rm -rf tmpsnaps; fi; mkdir tmpsnaps;"
 scp -P $port $snap  $user@$host:~/tmpsnaps/
 ssh ${SSH_OPTS} "sudo snap remove snapweb >/dev/null"
 ssh ${SSH_OPTS} "sudo snap install ~/tmpsnaps/$snap_name --devmode"
+ssh ${SSH_OPTS} 'echo "{DisableIPFilter: \"true\"}" > settings.json && sudo mv settings.json /var/snap/snapweb/common/settings.json'
+ssh ${SSH_OPTS} "sudo snap disable snapweb >/dev/null"
+ssh ${SSH_OPTS} "sudo snap enable snapweb >/dev/null"
 # need to manually connect interfaces as snapd won't do it anymore in devmode
 ssh ${SSH_OPTS} "sudo snap connect snapweb:snapd-control"
 ssh ${SSH_OPTS} "sudo snap connect snapweb:timeserver-control"
