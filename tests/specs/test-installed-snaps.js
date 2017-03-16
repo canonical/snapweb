de15f0dc4bd7e801917ddbd1b6a9b6380275f76d
Alexandre Abreu <abreu_alexandre@hotmail.com>
2017-03-16 19:36:39 -0400

    Tweak tests to account for conditional channel display (#209)

--- tests/specs/test-installed-snaps.js
+++ tests/specs/test-installed-snaps.js
@@ -89,7 +89,7 @@ describe('Installed Snaps Page - Verify that', function() {
         snap.click();
         browser.waitForVisible(snapDetailsPage.snapTitleElement);
         expect(snapDetailsPage.snap.getText(), "Failed to open snap's about page").to.equal(snap_name);
-        assert.isNotNull(snapDetailsPage.snapDetail(4).value, "Snap has no update date");
+        assert.isNotNull(snapDetailsPage.snapDetail(3).value, "Snap has no update date");
     });
 
     xit('snapweb updates the page when snap is installed/removed direclty on the device', function() {
