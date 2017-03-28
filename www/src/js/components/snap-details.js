e0df12cae55ec8e120303074799eb905c292c682
Alexandre Abreu <alexandre.abreu@canonical.com>
2017-03-28 14:49:19 -0400

    Tweaks & fixes

--- www/src/js/components/snap-details.js
+++ www/src/js/components/snap-details.js
@@ -1,51 +1,62 @@
 var Backbone = require('backbone');
 var React = require('react');
 var ReactBackbone = require('react.backbone');
+var CONF = require('../config.js');
 
 var Installer = require('./installer.js');
 
 function SnapSize(props) {
   var model = props.model;
+  var size = model.get('size');
+  if (!size) {
+    return null;
+  }
   return (
     <li className="p-list__item">
       Size
-      <span className="b-snap__size u-float--right">{model.get('size')}</span>
+      <span className="b-snap__size u-float--right">{size}</span>
     </li>
   );
 }
 
 function SnapVersion(props) {
   var model = props.model;
+  var version = model.get('version');
+  if (!version) {
+    return null;
+  }
   return (
     <li className="p-list__item">
       Version
-      <span className="b-snap__version u-float--right">{model.get('Version')}</span>
+      <span className="b-snap__version u-float--right">{version}</span>
     </li>
   );
 }
 
 function SnapChannel(props) {
   var model = props.model;
-  if (!model.get('channel')) {
+  var channel = model.get('channel');
+  if (!channel) {
     return null;
   }
   return (
     <li className="p-list__item">
       Channel
-      <span className="b-snap__version u-float--right">{model.get('channel')}</span>
+      <span className="b-snap__version u-float--right">{channel}</span>
     </li>
   );
 }
 
 function SnapInstallDate(props) {
   var model = props.model;
-  if (!model.get('install_date')) {
+  var installDate = model.get('install_date');
+  if (!installDate) {
     return null;
   }
   return (
     <li className="p-list__item">
       Updated
-      <span className="b-snap__version u-float--right">{{ install_date }}</span>
+      <span className="b-snap__version u-float--right">{installDate}</span>
     </li>
   );
 }
@@ -65,27 +76,33 @@ function About(props) {
   var model = props.model
   if (model.get('description')) {
     return (
-      <h3>About</h3>
-      <p>{model.get('description')}</p>
+      <div>
+        <h3>About</h3>
+        <p>{model.get('description')}</p>
+      </div>
     );
   }
   return (
-    <h3>About</h3>
-    <p><i>No description</i></p>
+    <div>
+      <h3>About</h3>
+      <p><i>No description</i></p>
+    </div>
   );
 }
 
 function SnapBanner(props) {
   var model = props.model;
   return (
-    <div className="col-2">
-      <div className="b-icon-wrapper">
-        <img className="b-icon" src={model.get('icon')} width="125" height="125" />
+    <div>
+      <div className="col-2">
+        <div className="b-icon-wrapper">
+          <img className="b-icon" src={model.get('icon')} width="125" height="125" />
+        </div>
       </div>
-    </div>
-    <div className="col-6">
-      <h1 className="b-snap__title">{model.get('name')}</h1>
-      <div className="b-snap__developer">{model.get('developer')}</div>
+      <div className="col-6">
+        <h1 className="b-snap__title">{model.get('name')}</h1>
+        <div className="b-snap__developer">{model.get('developer')}</div>
+      </div>
     </div>
   );
 }
@@ -104,6 +121,7 @@ function SnapActions(props) {
   var model = props.model;
   var progressWidth = props.downloadProgress + "%";
   var status = props.status;
+  var taskSummary = props.taskSummary;
 
   var progressBarBorder = "1px";
   var progressBarBorderRadius = "10%";
@@ -131,13 +149,13 @@ function SnapActions(props) {
         model={model} />
 
       <div id="progressbarwrapper"
-           style={{border: {progressBarBorder}; borderRadius: {progressBarBorderRadius}; marginTop: "1em"; width: "100%"; height: "0.5em"}}>
+           style={{border: {progressBarBorder}, borderRadius: {progressBarBorderRadius}, marginTop: "1em", width: "100%", height: "0.5em"}}>
         <div id="progress"
-             style={{backgroundColor: {progressBarBackgroundColor}; width: {progressWidth}; height: "0.5em"}} />
+             style={{backgroundColor: {progressBarBackgroundColor}, width: {progressWidth}, height: "0.5em"}} />
       </div>
 
-      <div style={{width: "100%"; height: "23px"; whiteSpace: "nowrap"; textOverflow: "ellipsis"; overflow: "hidden"}}>
-        <small id="tasksummary">{this.state.taskSummary}</small>
+      <div style={{width: "100%", height: "23px", whiteSpace: "nowrap", textOverflow: "ellipsis", overflow: "hidden"}}>
+        <small id="tasksummary">{taskSummary}</small>
       </div>
     </div>
   );
@@ -167,34 +185,36 @@ module.exports = React.createBackboneClass({
     var model = this.props.model;
 
     return (
-      <div className="p-strip--light">
-        <div className="row" style={{paddingBottom: "0"}}>
-          <SnapBanner model={model} />
-        </div>
+      <div>
+        <div className="p-strip--light">
+          <div className="row" style={{paddingBottom: "0"}}>
+            <SnapBanner model={model} />
+          </div>
 
-        <div className="row" style={{padding: "0"; paddingBottom: "0.5em"}}>
-          <div className="col-7">
-            <SnapActions
-              model={model}
-              downloadProgress={this.state.downloadProgress}
-              taskSummary={this.state.taskSummary}
-              status={this.state.status} />
+          <div className="row" style={{padding: "0", paddingBottom: "0.5em"}}>
+            <div className="col-7">
+              <SnapActions
+                model={model}
+                downloadProgress={this.state.downloadProgress}
+                taskSummary={this.state.taskSummary}
+                status={this.state.status} />
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="row">
-        <div className="col-7">
-          <SnapInfoDetails model={model} />
+        <div className="row">
+          <div className="col-7">
+            <SnapInfoDetails model={model} />
+          </div>
+          <div className="col-4">
+            <SnapInterfaces />
+          </div>
         </div>
-        <div className="col-4">
-          <SnapInterfaces />
-        </div>
-      </div>
 
-      <div className="row" style={{paddingBottom: "3em"}}>
-        <div className="col-8">
-          <About model={model} />
+        <div className="row" style={{paddingBottom: "3em"}}>
+          <div className="col-8">
+            <About model={model} />
+          </div>
         </div>
       </div>
     );
